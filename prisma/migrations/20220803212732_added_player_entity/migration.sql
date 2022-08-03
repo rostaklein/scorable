d0fb4105e0a43043b4dcfa1e17459434ed78c68c
Rostislav Klein <rostaklein@gmail.com>
2022-08-04 00:26:02 +0200

    migrated to mongo

--- prisma/migrations/20220803212732_added_player_entity/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Player" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "gameId" TEXT,
-    CONSTRAINT "Player_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
