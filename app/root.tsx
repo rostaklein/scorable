26004addc65bf87d7e5849fd0d0d68ec8083a7a4
Rostislav Klein <rostaklein@gmail.com>
2022-08-07 11:51:16 +0200

    PWA test

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useLocation, useMatches } from "@remix-run/react";
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
@@ -7,24 +9,60 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
 import styles from "./tailwind.css";
 
+let isMount = true;
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Pub Quiz Score",
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
       <head>
         <Meta />
+        <link rel="manifest" href="/resources/manifest.webmanifest" />
         <Links />
       </head>
       <body>
